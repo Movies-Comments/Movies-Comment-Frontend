0b680d038d6eed7c956e0f6f28cb0cf4c8b45fd7
canans <serpercanan@gmail.com>
2021-05-30 18:12:26 +0300

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MoviesComponent } from './movies/movies.component';
+import { PersonComponent } from './person/person.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AlertifyService } from './sevices/alertify.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MoviesComponent,
+    PersonComponent,
+    LoginComponent,
+    RegisterComponent,
+    LayoutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule,
+ 
   ],
-  providers: [],
+  providers: [
+    AlertifyService,
+   
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
